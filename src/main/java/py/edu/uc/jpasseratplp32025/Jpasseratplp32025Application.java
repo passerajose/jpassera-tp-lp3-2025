42a93e10fd2e7235e9e79447fa8d414ab8b0b3e9
passerajose <espinoza.esp123@gmail.com>
2025-09-13 18:48:07 -0300

    Initialize Spring Boot project with basic structure and configuration files

---
+++ src/main/java/py/edu/uc/jpasseratplp32025/Jpasseratplp32025Application.java
@@ -0,0 +1,13 @@
+package py.edu.uc.jpasseratplp32025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpasseratplp32025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpasseratplp32025Application.class, args);
+	}
+
+}
---
+++ src/test/java/py/edu/uc/jpasseratplp32025/Jpasseratplp32025ApplicationTests.java
@@ -0,0 +1,13 @@
+package py.edu.uc.jpasseratplp32025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpasseratplp32025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
